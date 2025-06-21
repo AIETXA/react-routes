d6f465a6d3c73a45ca39a7b32f8ccd91ffb420f3
Ailén <ailenechaguibel@gmail.com>
2025-06-21 11:59:54 +0200

    lista de proyectos ok traida, haciendo un poco de css

--- src/paginas/Projects.jsx
+++ src/paginas/Projects.jsx
@@ -1,13 +1,23 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
+import projects from '../data/projects'
 
 function Projects() {
 return (
-    <>
-    <p>Este componente mostrará la página de proyectos del portfolio. </p>
-    <p>En esta página, se mostrarán los proyectos en los que ha trabajado el desarrollador.</p>
-    </>
+    <div className='project-container'>
+    <h3>PROYECTOS DEL PORFOLIO.</h3>
+    <ul className='project-grid'>
+    {projects.map((project) => (
+        <li key={project.id} className='project-card'>
+        <h4>{project.name}</h4>
+        <img src={project.image} alt={project.name}/>
+        <p>{project.description}</p>
+        <a href={project.url} target="_blank" rel="url">Ver proyecto</a>
+        </li>
+    ))}
+    </ul>
+    </div>
 )
 }
 
